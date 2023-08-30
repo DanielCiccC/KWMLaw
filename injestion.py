833a2b997bd5e508c75d89a7370615a8b571213b
Daniel Ciccotosto-Camp <DanielCiccotostoCamp@nti.com.au>
2023-08-30 14:49:26 +1000

    injestion for .docx and .pdf working

--- injestion.py
+++ injestion.py
@@ -2,6 +2,9 @@ import imaplib
 import email
 import time
 from email.header import decode_header
+import fitz  # For handling PDFs, pip install PyMuPDF
+from docx import Document  # For handling Word documents
+from io import BytesIO
 
 # Your Gmail account details
 EMAIL = "kwmlaw@outlook.com"
@@ -13,6 +16,26 @@ SMTP_PORT = 993
 mail = imaplib.IMAP4_SSL(SMTP_SERVER)
 mail.login(EMAIL, PASSWORD)
 mail.select("inbox")  # You can select other mailboxes as well
+
+# Function to extract text from PDFs using PyMuPDF
+def extract_pdf_text(pdf_bytes):
+    pdf_io = BytesIO(pdf_bytes)
+    pdf_document = fitz.open("pdf", pdf_io.read())
+    text = ""
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document.load_page(page_num)
+        text += page.get_text("text")
+    pdf_document.close()
+    return text
+
+# Function to extract text from Word documents
+def extract_docx_text(docx_bytes):
+    docx_io = BytesIO(docx_bytes)
+    doc = Document(docx_io)
+    text = ""
+    for para in doc.paragraphs:
+        text += para.text + "\n"
+    return text
 
 # Infinite loop to check for new emails
 while True:
@@ -41,14 +64,22 @@ while True:
                             filename = part.get_filename()
                             if filename:
                                 print("Attachment:", filename)
-                        # else:
-                        #     text = part.get_payload(decode=True).decode()
-                        #     print("Message:", text)
+                                
+                        if "attachment" in content_disposition:
+                            filename = part.get_filename()
+                            if filename:
+                                attachment = part.get_payload(decode=True)
+                                if content_type == "application/pdf":
+                                    text = extract_pdf_text(attachment)
+                                elif content_type == "application/msword" or "vnd.openxmlformats-officedocument.wordprocessingml.document" in content_type:
+                                    text = extract_docx_text(attachment)
+                                else:
+                                    text = "Unsupported attachment type"
+                                print("Attachment Text:", text)
+                                
                 else:
-                    text = msg.get_payload(decode=True).decode()
-                    print("Message:", text)
-                
-                print("\n" + "=" * 40 + "\n")  # Separator between emails
-    time.sleep(5)
-# Logout and close the connection
+                    text = msg.get_payload(decode=True).decode()                
+                print("\n" + "=" * 40 + "\n") 
+    time.sleep(1)
+
 # mail.logout()
--- install.bat
+++ install.bat
@@ -1,1 +1,3 @@
 pip install python-imap
+pip install PyMuPDF
+pip install 
