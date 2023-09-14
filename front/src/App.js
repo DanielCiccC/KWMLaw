be7a33e5ea65de7434bef4788d6fa0b99211b3b9
MattPChoy <matthewpchoy@gmail.com>
2023-09-14 18:32:58 +1000

    Create-react-app template for new frontend iteration

---
+++ front/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+
+    </div>
+  )
+}
+
+function Main() {
+  return (
+    <div className='main'>
+
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
