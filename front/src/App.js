0d9457571b16a66e0a3cabd274ba4e6f8d815eb4
Michael CC <333mmcc@gmail.com>
2023-09-16 21:33:45 +1000

    Actually Daniel, testing out some radix-UI themes at Michaels desk

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
+import '@radix-ui/themes/styles.css';
+import { Avatar } from '@radix-ui/themes';
+import { Theme } from '@radix-ui/themes';
 import { ChatBubbleIcon, DotsHorizontalIcon, FontSizeIcon, HamburgerMenuIcon, LightningBoltIcon, PaperPlaneIcon } from '@radix-ui/react-icons';
 
 function Sidebar() {
   const [sessions, setSessions] = React.useState([
-    {summary: "Matter #1", dateTime: "01/09/2023 12:00 PM"},
-    {summary: "Matter #2", dateTime: "01/09/2023 12:00 PM"},
-    {summary: "Matter #3", dateTime: "01/09/2023 12:00 PM"},
-    {summary: "Matter #4", dateTime: "01/09/2023 12:00 PM"},
-    {summary: "Matter #5", dateTime: "01/09/2023 12:00 PM"},
+    { summary: "Matter #1", dateTime: "01/09/2023 12:00 PM" },
+    { summary: "Matter #2", dateTime: "01/09/2023 12:00 PM" },
+    { summary: "Matter #3", dateTime: "01/09/2023 12:00 PM" },
+    { summary: "Matter #4", dateTime: "01/09/2023 12:00 PM" },
+    { summary: "Matter #5", dateTime: "01/09/2023 12:00 PM" },
   ]);
 
   const pastChats = sessions.map((_session, index) => {
@@ -17,9 +20,9 @@ function Sidebar() {
     const dateTime = _session.dateTime;
     return (
       <div key={index} className='session'>
-        <div className='session-icon'><ChatBubbleIcon/></div>
+        <div className='session-icon'><ChatBubbleIcon /></div>
         <div className='session-text-container'>
-          <div className='session-name'>{session.substring(0,maxMsgLen) + (session.length > maxMsgLen ? "..." : "")}</div>
+          <div className='session-name'>{session.substring(0, maxMsgLen) + (session.length > maxMsgLen ? "..." : "")}</div>
           <span className='session-metadata'>{dateTime}</span>
         </div>
       </div>
@@ -30,25 +33,32 @@ function Sidebar() {
     <div className='sidebar'>
       <div className='top'>
         <div className='sidebarTopTray'>
-          <HamburgerMenuIcon className='hamburgerIcon'/>
+          <HamburgerMenuIcon className='hamburgerIcon' />
         </div>
         <div className='prevMessages'>{pastChats}</div>
       </div>
       <div className='userInfo'>
         <div className='userIcon'>PD</div>
         <div className='userName'>Patricia Daisy</div>
-        <DotsHorizontalIcon className='dotsIcon'/>
+        <DotsHorizontalIcon className='dotsIcon' />
       </div>
     </div>
   )
 }
 
-function Message({message, isSender}) {
+function Message({ message, isSender }) {
   return (
     <div className={`message-container ${isSender ? "sender" : "recipient"}`}>
       <div className='message-icon-container'>
-        {isSender ? <span className='message-icon'>PD</span> : <LightningBoltIcon className='message-icon'/>}
+        {isSender ? <span className='message-icon'>PD</span> : <LightningBoltIcon className='message-icon' />}
       </div>
+      <Theme>
+        {/* <Avatar
+          size="7"
+          src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
+          fallback="S"
+        /> */}
+      </Theme>
       <div className='message-body'>
         <p>{message}</p>
       </div>
@@ -56,22 +66,24 @@ function Message({message, isSender}) {
   )
 }
 
+
+
 function Main() {
   const [messages, setMessages] = React.useState([
-    {body: "Have you recently experienced or witnessed a highly distressing or life-threatening event, such as a natural disaster, accident, assault, or military combat?", isSender: false},
-    {body: "No, I haven't recently experienced or witnessed a highly distressing or life-threatening event.", isSender: true},
-    {body: "Are you frequently reliving or experiencing intrusive thoughts, memories, or nightmares related to a traumatic event?", isSender: false},
+    { body: "Have you recently experienced or witnessed a highly distressing or life-threatening event, such as a natural disaster, accident, assault, or military combat?", isSender: false },
+    { body: "No, I haven't recently experienced or witnessed a highly distressing or life-threatening event.", isSender: true },
+    { body: "Are you frequently reliving or experiencing intrusive thoughts, memories, or nightmares related to a traumatic event?", isSender: false },
   ]);
 
   const messageElements = messages.map((message, index) => {
     return (
-      <Message key={index} message={message.body} isSender={message.isSender}/>
+      <Message key={index} message={message.body} isSender={message.isSender} />
     )
   });
 
   const handleInput = (e) => {
     console.log(e.target)
-    setMessages([...messages, {body: document.getElementById('msg_input').value, isSender: true}]);
+    setMessages([...messages, { body: document.getElementById('msg_input').value, isSender: true }]);
     e.target.value = '';
   }
 
@@ -83,14 +95,14 @@ function Main() {
 
   return (
     <div className='main'>
-        <div className='chatMessages'>
-          {messageElements}
-        </div>
+      <div className='chatMessages'>
+        {messageElements}
+      </div>
 
-        <div className='chatInput'>
-          <input className='chatInputField' id='msg_input' type='text' placeholder='Type a message...' onKeyDown={handleKeyDown}/>
-          <PaperPlaneIcon className="chatInputButton" onClick={handleInput}/>
-        </div>
+      <div className='chatInput'>
+        <input className='chatInputField' id='msg_input' type='text' placeholder='Type a message...' onKeyDown={handleKeyDown} />
+        <PaperPlaneIcon className="chatInputButton" onClick={handleInput} />
+      </div>
     </div>
   )
 }
@@ -100,14 +112,14 @@ function App() {
   return (
     <div className="App">
       <div className="title">
-        <span className='appDescription'> 
+        <span className='appDescription'>
           <span className='appName'>Lex •</span>
           <i>&nbsp; Your legal compassion companion</i>
         </span>
       </div>
       <div className='mainContent'>
-        <Sidebar/>
-        <Main/>
+        <Sidebar />
+        <Main />
       </div>
     </div>
   );
